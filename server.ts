7d8c1ab41bce8f6eeaac30edb1957c9a38d1f55e
seungyeop Kim <seungyeop2002@gmail.com>
2020-06-27 21:56:35 +0900

    Connect prisma client with graphQL server

--- server.ts
+++ server.ts
@@ -1,21 +1,36 @@
 import { GraphQLServer } from 'graphql-yoga';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const typeDefs = `
   type Query {
     hello: String!
+    users: [User]!
   }
 
+  type User {
+    id: ID!
+    email: String
+    name: String
+  }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello!'
-  }
+    hello: () => 'Hello!',
+    users: async (root: any, args: any, context: any) => {
+      const users = await context.user.findMany();
+      console.log(users);
+      return users;
+    }
+  },
 };
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
+  context: prisma,
 });
 
 server.start({ port: 8000 }, () => {
